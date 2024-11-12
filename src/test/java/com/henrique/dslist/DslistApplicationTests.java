e1126563386cd6f73f96637040b14fbd1cda03c9
Henrique <henriquepg1011@gmail.com>
2024-11-11 23:17:09 -0300

    Project created

---
+++ src/test/java/com/henrique/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
